55fe0a420c7ac816e3b9250d5ecedcfdb4e8ab41
nejishow <ashrafmuhamad2014@gmail.com>
2020-04-17 19:03:21 +0300

    heroku

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const commandRouter = require("./routers/command")
 const app = express()
 const port = process.env.PORT || 3000
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "practical-mcnulty-9efd03.netlify.app");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Authorization,  Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, PATCH, DELETE, POST");
   next();
